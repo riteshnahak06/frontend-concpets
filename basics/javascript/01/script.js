ea2445e941520014b6a593c111883ad2381a8443
nahakritesh06 <nahakritesh06@gmail.com>
2026-02-03 23:26:51 +0530

    Intial Commit

--- basics/javascript/01/script.js
+++ basics/javascript/01/script.js
@@ -39,7 +39,7 @@ if (birthYear <= 2000) {
 console.log(century);
 */
 // type conversion
-
+/*
 const input = '1991';
 console.log(input + 18);
 console.log(Number(input) + 18);
@@ -47,3 +47,61 @@ console.log(Number(input) + 18);
 const textInput = 'Adc';
 console.log(Number(textInput) + 18); // NaN - Not a number means invalid number
 console.log(typeof NaN); // number
+
+// equality operator
+const age = 18;
+if (age === 18) console.log('You are an adult :D');
+*/
+
+// Task
+
+/*
+There are two gymnastics teams: Dolphins and Koalas. They compete against each other 3 times. The winner with the highest average score wins a trophy!
+
+Your tasks:
+1. Calculate the average score for each team, using the test data included below. The average score for Dolphins should be assigned to the scoreDolphins variable, and the average score of Koalas should be assigned to the scoreKoalas variable.
+
+2. Compare the team's average scores to determine the winner of the competition, and print to the console:
+"Dolphins win the trophy" if Dolphins win, or
+"Koalas win the trophy" if Koalas win, or
+"Both win the trophy" if their average scores are equal.
+TEST DATA: Dolphins scored 96, 108, and 89. Koalas scored 88, 91, and 110.
+*/
+
+const scoreDolphins = (96 + 108 + 89) / 3;
+const scoreKoalas = (88 + 91 + 110) / 3;
+
+console.log(scoreDolphins, scoreKoalas);
+
+if (scoreDolphins > scoreKoalas) {
+  console.log('Dolphins win the trophy');
+} else if (scoreDolphins < scoreKoalas) {
+  console.log('Koalas win the trophy');
+} else {
+  console.log('Both win the trophy');
+}
+
+/*
+Steven needs a very simple tip calculator for whenever he goes to eat in a restaurant. In his country, it's usual to tip 15% if the bill value is between 50 and 300. If the value is different, the tip is 20%.
+
+Your tasks:
+Calculate the tip, depending on the bill value. Create a variable called tip for this. It's not allowed to use an if...else statement (if it's easier for you, you can start with an if...else statement, and then try to convert it to a ternary operator).
+
+Print a string to the console containing the bill value, the tip, and the final value (bill + tip).
+
+Example: The bill was 275, the tip was 41.25, and the total value 316.25.
+
+Note: Use the values of the bill and tip variables to construct this string. Don't hard-code them 🙂
+
+TEST DATA: Test with different bill values: 275, 40, and 430
+
+HINT: To calculate 20% of a value, simply multiply it by 20/100 = 0.2
+
+HINT 2: Value X is between 50 and 300, if it's >= 50 && <= 300 😉
+*/
+
+const bill = 275;
+let tip = bill >= 50 && bill <= 300 ? 0.15 * bill : 0.2 * bill;
+console.log(
+  `The bill was ${bill}, the tip was ${tip}, and the total value ${bill + tip}`,
+);
